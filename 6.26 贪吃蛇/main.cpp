ef0524ee2e8c7ae93d6bd3b7eeb9c1ce4a61a2fc
20171105128 <s20171105128@admindeimac-88.local>
2018-06-26 14:20:36 +0800

    Initial Commit

---
+++ 6.26 贪吃蛇/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  6.26 贪吃蛇
+//
+//  Created by TAO on 18/6/26.
+//  Copyright © 2018年 TAO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
